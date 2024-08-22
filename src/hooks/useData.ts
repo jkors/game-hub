74747af29c6eaceecd9afe01f22a43260b3eb218
Jarl Korsvik <jarl.korsvik@online.no>
2024-08-22 18:43:54 +0200

    Filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,36 +1,46 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useGenres = <T>(endpoint: string) => {
+const useGenres = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState(null);
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-      .then((response) => {
-        setData(response.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      setLoading(true);
+      apiClient
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((response) => {
+          setData(response.data.results);
+          setLoading(false);
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setError(error.message);
+          setLoading(false);
+        });
 
-    return () => controller.abort(); // Clean up the controller when the component unmounts
+      return () => controller.abort(); // Clean up the controller when the component unmounts
 
-    // Only fetch new data if the error state changes
-  }, []);
+      // Only fetch new data if the error state changes
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, isLoading, error };
 };
